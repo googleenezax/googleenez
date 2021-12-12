3dc30249f8690b163bb02ecc78ef283b144caeb8
Nhat Hao <tranlenhathao2000@gmail.com>
2021-12-12 23:32:02 +0700

    updated

---
+++ truyenmoi/src/home.js
@@ -0,0 +1,9 @@
+function execute() {
+    return Response.success([
+        {title: "Truyện mới cập nhật", input: "https://truyenmoiz.com/danh-sach/truyen-moi", script: "gen.js"},
+		{title: "Truyện HOT", input: "https://truyenmoiz.com/danh-sach/truyen-hot", script: "gen.js"},
+		{title: "Truyện FULL", input: "https://truyenmoiz.com/danh-sach/truyen-full", script: "gen.js"},
+		{title: "Truyện mới", input: "https://truyenmoiz.com/the-loai/truyen-audio", script: "gen.js"}
+		
+    ]);
+}
